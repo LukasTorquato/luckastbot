7f84b2b94426e19d0c48fd27882b8a550a2e5e4a
Lukas <lukasmtorquato@gmail.com>
2021-02-25 00:03:11 -0300

    files

--- bnapi.py
+++ bnapi.py
@@ -1,22 +1,27 @@
 # import ta-lib
+# import tradingview-ta
 import csv
 import websocket
 from config import *
 from binance.client import Client
 from binance.enums import *
+from tradingview_ta import TA_Handler, Interval, Exchange
+
 
 # Conecta API
 client = Client(API_KEY, API_SECRET)
-
+'''
 # prices = client.get_all_tickers()  # Pega todos os preços de todos os pares
 
 orders = client.get_all_orders(symbol='ADABUSD')
 # cancelorder = client.cancel_order(
 #     symbol='ADABUSD', orderId=orders[0]['orderId'])
+
 for order in orders:
     print(order)
 
-'''csvfile = open('candles.csv', 'w', newline='')
+'''
+csvfile = open('datasets/BTCUSDT.csv', 'w', newline='')
 
 fieldnames = ['Time Init', 'Open', 'High', 'Low',
               'Close', 'Volume', 'Time Close', 'QAV', 'N Trades']
@@ -24,9 +29,9 @@ writer = csv.writer(csvfile, delimiter=',')
 writer.writerow(fieldnames)
 
 klines = client.get_historical_klines(
-    "BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Jan, 2012", "1 Jan, 2021")  # pega o candle do intervalo de tempo
+    "BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 Jan, 2012", "23 Feb, 2021")  # pega o candle do intervalo de tempo
 
 for kline in klines:
-    writer.writerow(kline)
+    writer.writerow(kline[0:9])
 
-csvfile.close()'''
+csvfile.close()
