fb3f52cb0f2abbfbab5f23708aa123a382c48182
Lukas <lukasmtorquato@gmail.com>
2021-03-06 15:27:24 -0300

    piru

--- bnapi.py
+++ bnapi.py
@@ -1,5 +1,7 @@
 import csv
 import websocket
+import numpy as np
+from ta import *
 from config import *
 from binance.client import Client
 from binance.enums import *
@@ -19,17 +21,24 @@ for order in orders:
     print(order)
 
 '''
-csvfile = open('datasets/ETHUSDT.csv', 'w', newline='')
+csvfile = open('datasets/BTCUSDT-1D-RSI.csv', 'w', newline='')
 
 fieldnames = ['Time Init', 'Open', 'High', 'Low',
-              'Close', 'Volume', 'Time Close', 'QAV', 'N Trades']
+              'Close', 'Volume', 'Time Close', 'QAV', 'N Trades', 'RSI']
 writer = csv.writer(csvfile, delimiter=',')
 writer.writerow(fieldnames)
 
 klines = client.get_historical_klines(
-    "ETHUSDT", Client.KLINE_INTERVAL_4HOUR, "1 Jan, 2012", "23 Feb, 2021")  # pega o candle do intervalo de tempo
+    "BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Jan, 2012", "5 Mar, 2021")  # pega o candle do intervalo de tempo
+
+
+klines = np.asarray(klines, dtype=np.float64)
+
+rsi = np.asarray(ta_rsi(klines[:, 4]), dtype=np.float64)
+
+klines = np.column_stack((klines[:, 0:9], rsi))
 
 for kline in klines:
-    writer.writerow(kline[0:9])
+    writer.writerow(kline[0:10])
 
 csvfile.close()
--- ta.py
+++ ta.py
@@ -1,16 +1,17 @@
 import talib
 import csv
 import numpy as np
+import pandas as pd
 
 
-def ta_rsi(open, high, low, close):
+def ta_rsi(close):
     #rsi = np.array()
     rsi = talib.RSI(close, timeperiod=14)
-    return rsi
+    return np.around(rsi, decimals=6)
 
 
-data = np.genfromtxt('datasets/BTCUSDT-15MIN.csv', delimiter=',')
+#data = np.genfromtxt('datasets/BTCUSDT-1D.csv', delimiter=',')
 
-rsi = ta_rsi(data[:, 1], data[:, 2], data[:, 3], data[:, 4])
+#rsi = ta_rsi(data[:, 1], data[:, 2], data[:, 3], data[:, 4])
 
-print(rsi)
+# print(rsi)
